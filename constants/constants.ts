d599ad1d4101ec01a02f7aa7ba9935c41f0a8f39
Alexander <ramonalaurencio5@gmail.com>
2026-07-31 22:18:06 -0400

    Updated the host allowed

--- constants/constants.ts
+++ constants/constants.ts
@@ -3,7 +3,7 @@ export const port = process.env.PORT || 5000;
 
 export const server = `http://localhost:${port}`;
 
-export const hostAllowed = "http://localhost:5173";
+export const hostAllowed = "https://alexanderlaurencio.github.io/task-manager-app";
 
 export const CONTENT_TYPE = {
     text: "text/plain",
